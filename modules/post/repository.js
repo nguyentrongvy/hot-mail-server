2d319e20e9a3e074fa5c5c644c783439fd38d50d
nguyentrongvy15 <vy.nguyen@emandai.net>
2022-04-24 12:41:40 +0700

    test pr

---
+++ modules/post/repository.js
@@ -0,0 +1,11 @@
+// @ts-check
+
+const BaseRepository = require('../../common/base-repository');
+
+class PostRepository extends BaseRepository {
+    constructor() {
+        super('Post');
+    }
+}
+
+module.exports = new PostRepository();
